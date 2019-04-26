56b73b32a2d6cbf54635238e1d1fe2a3a5b79ec1
Jonathan Ray <jonathanray17@gmail.com>
2019-04-26 09:56:43 -0400

    cleans up code

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,10 +1,8 @@
 import React from 'react';
 function Stopwatch({timerTime, clickHandler, timerBool}){
-    let minutes = Math.floor(timerTime/60)%60;
-
     return(<>
         <h2>{Math.floor(timerTime/3600)} hours</h2>
-        <h3>{minutes} minutes</h3>
+        <h3>{Math.floor(timerTime/60)%60} minutes</h3>
         <h4>{timerTime%60} seconds</h4>
         <button onClick={clickHandler}>{timerBool? `Stop` : `Start`}</button>
     </>);
